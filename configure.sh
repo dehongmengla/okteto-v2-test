1c014d7f7db45ccd68adf204911b1d78cb056cbf
dehongmengla <76235607+dehongmengla@users.noreply.github.com>
2020-12-19 10:48:24 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,35 +2,37 @@
 #显示时间
 date
 cat << EOF > /usr/local/etc/v2ray/config.json
-{
-  "inbounds": [
-  {
-    "port": 9090,
-    "listen":"127.0.0.1",
-    "protocol": "vmess",
-    "settings": {
-      "clients": [
-        {
-          "id": "ad806487-2d26-4636-98b6-ab85cc8521f7",
-          "alterId": 64       
+ {
+          "log": {
+            "access": "none",
+            "loglevel": "error"
+          },
+          "inbounds": [
+            {
+              "port": 9090,
+              "protocol": "vless",
+              "settings": {
+                "decryption": "none",
+                "clients": [
+                  {
+                    "id": "ad806487-2d26-4636-98b6-ab85cc8521f7"
+                  }
+                ]
+              },
+              "streamSettings": {
+                "network":"ws",
+                "wsSettings": {
+                  "path": "/"
+                }
+              }
+            }
+          ],
+          "outbounds": [
+            {
+              "protocol": "freedom"
+            }
+          ]
         }
-      ]
-    },
-    "streamSettings": {
-      "network": "ws",
-      "wsSettings": {
-      "path": "/"
-      }     
-    }
-  }
-  ],
-  "outbounds": [
-  {
-    "protocol": "freedom",
-    "settings": {}
-  }
-  ]
-}
 EOF
 # Run V2Ray
 VERSION=$(/usr/local/bin/v2ray --version |grep V |awk '{print $2}')
